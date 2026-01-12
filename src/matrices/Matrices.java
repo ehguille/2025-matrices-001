b5103bce97681a626110e88196f1a604f8888a8d
guillermogb <guillermogb@profe208b.informatica.cifpaviles.pa>
2026-01-12 10:36:42 +0100

    No puedo estar paradín.

--- src/matrices/Matrices.java
+++ src/matrices/Matrices.java
@@ -1,5 +1,19 @@
 package matrices;
 
 public class Matrices {
+	
+	public int[][] crearMatrizAleatoria(int filas, int columnas, int valorMinimo, int valorMaximo){
+		int[][] matriz=new int[filas][columnas];
+		/*TODO: rellenar esto con números aleatorios.
+		for(para recorrer las filas)
+			for(para recorrer las columnas)
+				matriz[i][j]=generarAleatorio(valorMinimo,valorMaximo);
+		*/
+		return matriz;
+	}
+	
+	private int generarAleatorio(int valorMinimo, int valorMaximo) {
+		return 0;
+	}
 
 }
