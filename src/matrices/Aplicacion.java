04abb676c2e799319d8fa51cd327a87eea617e8d
guillermogb <guillermogb@profe208b.informatica.cifpaviles.pa>
2026-01-15 10:22:35 +0100

    Clase del 15/01

--- src/matrices/Aplicacion.java
+++ src/matrices/Aplicacion.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Aplicacion {
 	
 	private int valorMinimo=0;
-	private int valorMaximo=10;
+	private int valorMaximo=3;
 	
 	public Aplicacion() {
 		Scanner s=new Scanner(System.in);
@@ -14,6 +14,8 @@ public class Aplicacion {
 			System.out.println("[Aplicación para operar con matrices]");
 			System.out.println("1: Sumar matrices.");
 			System.out.println("2: Multiplicar matrices.");
+			System.out.println("3: Poblar matriz aleatoriamente.");
+			System.out.println("99: Ejemplo excepciones");
 			System.out.println("Otra opción: salir.");
 			System.out.println("Escoja una opción del menú:");
 			opcion=s.nextInt();
@@ -41,9 +43,48 @@ public class Aplicacion {
 					System.err.println(e.getMessage());
 				}				
 
-				break;		
+				break;	
+			case 2:
+				System.out.println("Introduce el número de filas de la matriz A.");
+				int filasMatrizA=s.nextInt();
+				System.out.println("Introduce el número de columnas de la matriz A.");
+				int columnasMatrizA=s.nextInt();
+				
+				int[][] matrizAMultiplicando=Matrices.crearMatrizAleatoria(filasMatrizA, columnasMatrizA, valorMinimo, valorMaximo);
+				System.out.println("Matriz A:");
+				Matrices.imprimirMatriz(matrizAMultiplicando);
+				
+				System.out.println("Introduce el número de filas de la matriz B.");
+				int filasMatrizB=s.nextInt();
+				System.out.println("Introduce el número de columnas de la matriz B.");
+				int columnasMatrizB=s.nextInt();
+				
+				int[][] matrizBMultiplicando=Matrices.crearMatrizAleatoria(filasMatrizB, columnasMatrizB, valorMinimo, valorMaximo);
+				System.out.println("Matriz B:");
+				Matrices.imprimirMatriz(matrizBMultiplicando);
+				
+				Matrices.multiplicar(matrizAMultiplicando, matrizBMultiplicando);
+				break;
+			case 3:
+				//TODO: Pedir parámetros
+			//	System.out.println("Introduce la probabilidad con la que quieres que se poble con 1:");
+			//	int probabilidad=s.nextInt();
+				Matrices.poblarMatriz(1000,1000,50);
+				break;
+			case 99:
+				System.out.println("Voy a crear un array de 10 posiciones. Introduce un valor fuera del array:");
+				int[] arrayExcepcion= {0,0,0,0,0,0,0,0,0,0};
+				int i=s.nextInt();
+				System.out.println("Accediendo a la posición "+i);
+				try {
+					System.out.println(arrayExcepcion[i]);
+				} catch(Exception e) {
+					System.err.println("El valor está fuera de los límites del array.");
+					e.printStackTrace();
+				}				
+				break;
 			}
-		} while(opcion==1||opcion==2);
+		} while(opcion==1||opcion==2||opcion==3||opcion==99);
 
 	}
 	
--- src/matrices/Matrices.java
+++ src/matrices/Matrices.java
@@ -4,12 +4,18 @@ import java.util.Random;
 
 public abstract class Matrices {
 	
-	//TODO: Excepción si filas o columnas <1...
+	//TODO: Excepción si filas o columnas <1....
 	public static int[][] crearMatrizAleatoria(int filas, int columnas, int valorMinimo, int valorMaximo){
 		int[][] m=new int[filas][columnas];
 		for(int i=0;i<m.length;i++) 
-			for(int j=0;j<m[i].length;j++)	
-				m[i][j]=generarAleatorio(valorMinimo,valorMaximo);
+			for(int j=0;j<m[i].length;j++)	{
+				try {
+					m[i][j]=generarAleatorio(valorMinimo,valorMaximo);
+				} catch(Exception e) {
+					System.err.println(e.getMessage());
+					System.err.println("La aplicación sigue ejecutándose a pesar del error (algo que es un error desde el punto de vista de diseño de la aplicación.");
+				}
+			}
 		return m;
 	}
 	
@@ -38,13 +44,52 @@ public abstract class Matrices {
 		return m;
 	}
 	
+	public static int[][] multiplicar(int[][] matrizA, int[][] matrizB){
+		int[][] resultado=new int[matrizA.length][matrizB[0].length];
+		//Dos bucles para recorrer la matriz resultado
+		for(int i=0;i<resultado.length;i++) {
+			for(int j=0;j<resultado[0].length;j++) {
+				for(int k=0;k<matrizA.length;k++) {
+					resultado[i][j]+=matrizA[i][k]*matrizB[k][j];
+				}				
+			}
+		}
+		Depurador.imprimirTraza("Resultado de la multiplicación:");
+		imprimirMatriz(resultado);
+		return resultado;
+	}
+	
 	//TODO: Excepción si valorMinimo>valorMaximo.
-	private static int generarAleatorio(int valorMinimo, int valorMaximo) {
-		//Ignorad la siguiente línea.
-		assert valorMinimo<valorMaximo : "El mínimo no puede ser mayor que el máximo";
+	private static int generarAleatorio(int valorMinimo, int valorMaximo) throws Exception {
+		if(valorMinimo>=valorMaximo)
+			throw new Exception("El mínimo no puede ser mayor que el máximo");
 		Random r=new Random();
 		return r.nextInt(valorMinimo,valorMaximo);
 	}
 	
+	public static int[][] poblarMatriz(int filas, int columnas, int probabilidad) {
+		int[][] resultado=new int[filas][columnas];
+		int numeroCeros=0;
+		int numeroUnos=0;
+		for(int i=0;i<resultado.length;i++)
+			for(int j=0;j<resultado[0].length;j++) {
+				try {
+					int numeroAleatorio=generarAleatorio(0,100);
+					if(numeroAleatorio<probabilidad) {
+						resultado[i][j]=1;
+						numeroUnos++;
+					}					
+					else {
+						resultado[i][j]=0;
+						numeroCeros++;
+					}
+				} catch(Exception e) {
+					System.err.println(e.getMessage());
+				}
+			}
+		Depurador.imprimirTraza("Número de ceros: "+numeroCeros);
+		Depurador.imprimirTraza("Número de unos: "+numeroUnos);
+		return resultado;
+	}	
 
 }
